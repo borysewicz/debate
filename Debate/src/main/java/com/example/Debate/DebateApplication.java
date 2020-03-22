0da71dacaf0e029ce0fc9d1bb69cb45ea5a6deae
Mateusz Koza <32997806+Chrumcio@users.noreply.github.com>
2020-03-22 13:20:00 +0100

    Created empty spring project with HelloWorldController

---
+++ Debate/src/main/java/com/example/Debate/DebateApplication.java
@@ -0,0 +1,13 @@
+package com.example.Debate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebateApplication.class, args);
+	}
+
+}
---
+++ Debate/src/main/java/com/example/Debate/controller/HelloWorldController.java
@@ -0,0 +1,15 @@
+package com.example.Debate.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Hello")
+public class HelloWorldController {
+
+    @GetMapping
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
