fe2d7ceb64bc65e03aeb01c2e06f9f93b4682b28
Marcin Drabek <32941183+Drabenstein@users.noreply.github.com>
2020-03-31 20:49:08 +0200

    Apply suggestions from code review

--- sources/Debate-frontend/src/app/app.module.ts
+++ sources/Debate-frontend/src/app/app.module.ts
@@ -4,13 +4,13 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatTabsModule } from '@angular/material/tabs'; 
-import {MatCardModule} from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card'; 
 import { MatGridListModule } from '@angular/material/grid-list'; 
 import { MatChipsModule } from '@angular/material/chips'; 
 
