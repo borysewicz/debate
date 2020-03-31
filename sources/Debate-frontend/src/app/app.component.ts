9bb0e6eb4d686b001a94e0ef3abe41e7d687f307
Marcin Drabek <marcindrabek@hotmail.com>
2020-04-01 01:27:56 +0200

    Add complete homepage component and main app layout

--- sources/Debate-frontend/src/app/app.component.ts
+++ sources/Debate-frontend/src/app/app.component.ts
@@ -1,18 +1,36 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { map, startWith } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  debateSearch = new FormGroup({
-    debate: new FormControl('')
-  });
-  debateSearchDummy: string[] = [
+export class AppComponent implements OnInit {
+  debateControl = new FormControl('');
+  debates: string[] = [
     'Wykłady to marnowanie czasu',
     'Dostęp do broni powinien być powszechny',
     'Aborcja powinna być legalna'
   ];
+  filteredDebates: Observable<string[]>;
+
+  ngOnInit(): void {
+    this.filteredDebates = this.debateControl.valueChanges.pipe(
+      // tslint:disable-next-line: deprecation
+      startWith(null),
+      map((debate: string | null) =>
+        debate ? this._filter(debate) : this.debates.slice()
+      )
+    );
+  }
+
+  private _filter(newTag: string) {
+    const filterValue = newTag.toLowerCase();
+    return this.debates.filter(debate =>
+      debate.toLowerCase().includes(filterValue)
+    );
+  }
 }
--- sources/Debate-frontend/src/app/app.module.ts
+++ sources/Debate-frontend/src/app/app.module.ts
@@ -1,21 +1,23 @@
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatIconModule } from "@angular/material/icon";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { BrowserModule } from "@angular/platform-browser";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { MatAutocompleteModule } from "@angular/material/autocomplete";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import {MatTabsModule} from '@angular/material/tabs'; 
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatChipsModule} from '@angular/material/chips';
+import {MatTabsModule} from '@angular/material/tabs';
 
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TagSearchComponent } from './tag-search/tag-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HomepageComponent],
+  declarations: [AppComponent, HomepageComponent, TagSearchComponent],
   imports: [
     BrowserModule,
     MatToolbarModule,
@@ -25,6 +27,7 @@ import { HomepageComponent } from './homepage/homepage.component';
     MatAutocompleteModule,
     MatInputModule,
     MatFormFieldModule,
+    MatChipsModule,
     MatTabsModule,
     FormsModule,
     ReactiveFormsModule,
