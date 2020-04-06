8623436a9f9b9e2f319c36796655fb1922e5af72
= <mriperm@gmail.com>
2020-04-06 15:24:57 +0200

    Add exception handling

--- sources/Debate/src/main/java/com/example/Debate/dto/ActivityDto.java
+++ sources/Debate/src/main/java/com/example/Debate/dto/ActivityDto.java
@@ -1,10 +1,5 @@
 package com.example.Debate.dto;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
 public abstract class ActivityDto {
     protected String _id;
     protected Long creationDate;
--- sources/Debate/src/main/java/com/example/Debate/service/DebateService.java
+++ sources/Debate/src/main/java/com/example/Debate/service/DebateService.java
@@ -5,7 +5,7 @@ import com.example.Debate.dto.DebateDto;
 import java.util.List;
 
 public interface DebateService {
-    public DebateDto getDebateById(String id);
-    public List<DebateDto> getAllDebates();
-    public boolean addDebate(DebateDto debateDto);
+    DebateDto getDebateById(String id);
+    List<DebateDto> getAllDebates();
+    boolean addDebate(DebateDto debateDto);
 }
