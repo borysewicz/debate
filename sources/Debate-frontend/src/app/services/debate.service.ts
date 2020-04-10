b5e41438b2a563b723592dd1ea1a6024d2be6bab
= <mriperm@gmail.com>
2020-04-10 13:17:19 +0200

    Refactor debate service

--- sources/Debate-frontend/src/app/services/debate.service.ts
+++ sources/Debate-frontend/src/app/services/debate.service.ts
@@ -13,26 +13,23 @@ export class DebateService {
   constructor(private http: HttpClient) {}
 
   getPopularDebates(limit: number, page: number): Observable<Debate[]> {
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append('sort', 'popular');
-    queryParams = queryParams.append('limit', limit.toString());
-    queryParams = queryParams.append('page', page.toString());
-    return this.http.get<Debate[]>(this.endpoint, { params: queryParams});
+    return this.getDebates('popular', limit ,page);
   }
 
   getNewDebates(limit: number, page: number): Observable<Debate[]> {
+    return this.getDebates('new', limit, page);
+  }
+
+  getHotDebates(limit: number, page: number): Observable<Debate[]> {
+    return this.getDebates('hot', limit, page);
+  }
+
+  private getDebates(sort: string, limit: number, page: number): Observable<Debate[]> {
     let queryParams = new HttpParams();
-    queryParams = queryParams.append('sort', 'new');
+    queryParams = queryParams.append('sort', sort);
     queryParams = queryParams.append('limit', limit.toString());
     queryParams = queryParams.append('page', page.toString());
     return this.http.get<Debate[]>(this.endpoint, { params: queryParams});
   }
 
-  getHotDebates(limit: number, page: number): Observable<Debate[]> {
-    let queryParams = new HttpParams();
-    queryParams = queryParams.append('sort', 'hot');
-    queryParams = queryParams.append('limit', limit.toString());
-    queryParams = queryParams.append('page', page.toString());
-    return this.http.get<Debate[]>(this.endpoint, { params: queryParams});
-  }
 }
