a135c1b93d8296d2e8489a038969b3e52631f783
Marcin Drabek <marcindrabek@hotmail.com>
2020-04-28 16:07:01 +0200

    Add report menu to arguments and comments

--- sources/Debate-frontend/src/app/app.module.ts
+++ sources/Debate-frontend/src/app/app.module.ts
@@ -1,35 +1,35 @@
+import { TextFieldModule } from '@angular/cdk/text-field';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatTabsModule } from '@angular/material/tabs';
-import { MatCardModule } from '@angular/material/card';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatChipsModule } from '@angular/material/chips';
-import { TextFieldModule } from '@angular/cdk/text-field';
-import { MatListModule } from '@angular/material/list';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatButtonToggleModule } from '@angular/material/button-toggle';
-
-import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { DebateCardComponent } from './homepage/debate-card/debate-card.component';
-import { TagSearchComponent } from './tag-search/tag-search.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 import { AddUpdateDebateComponent } from './debate/add-update-debate/add-update-debate.component';
-import { DebateComponent } from './debate/debate.component';
-import { DebateArgumentComponent } from './debate/debate-argument/debate-argument.component';
 import { CommentComponent } from './debate/comment/comment.component';
+import { DebateArgumentComponent } from './debate/debate-argument/debate-argument.component';
+import { DebateComponent } from './debate/debate.component';
+import { DebateCardComponent } from './homepage/debate-card/debate-card.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { TagSearchComponent } from './tag-search/tag-search.component';
 
 @NgModule({
   declarations: [
@@ -58,6 +58,7 @@ import { CommentComponent } from './debate/comment/comment.component';
     MatButtonToggleModule,
     MatCardModule,
     MatProgressBarModule,
+    MatMenuModule,
     MatGridListModule,
     HttpClientModule,
     FormsModule,
