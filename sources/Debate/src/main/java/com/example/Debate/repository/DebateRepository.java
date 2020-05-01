234412a32bc23483db7aa08b69fadbfe573ce353
CzyzuM <44680508+CzyzuM@users.noreply.github.com>
2020-05-01 17:16:56 +0200

    Mczyz/search for debate (#16)
    * Initial commit
    
    add SearchController
    add draft SearchService
    
    * Add search by name
    
    Using a single case insensitive repo-generated query
    
    * Add search by tags
    
    With sorting in order of matching tags count.
    
    * Update search by tags
    
    Make incoming tags lower case
    Transform incoming List into a HashSet to optimize searching
    
    * Major cleanup
    
    * Restoring mistakenly deleted files
    
    * Actually repairing everything to a state 4 commits back. Not funny.
    
    * Apply first pull request review changes
    
    * Add angular.json after npm ci
    
    * Revert "Add angular.json after npm ci"
    
    This reverts commit 3775d66abf765136a346cbec714a6b0aa253b34b.
    
    git revert testing
    
    * Revert "Apply first pull request review changes"
    
    This reverts commit 3574af47f2e890d0583def9e068ab6ade418947d.
    
    * Revert "Merge branch 'dev' into mczyz/searchForDebate"
    
    This reverts commit 293e1ab79f04f266855a8a4b98403100744aabe6, reversing
    changes made to 5ba41364b95bbb20793600d5e766b8ee75beb1ba.
    
    * Revert "Actually repairing everything to a state 4 commits back. Not funny."
    
    This reverts commit 5ba41364b95bbb20793600d5e766b8ee75beb1ba.
    
    * Revert "Restoring mistakenly deleted files"
    
    This reverts commit d9ff7a16f09fb2abf387cf8e338217949f070f87.
    
    reverting faulty commits
    
    * Revert "Major cleanup"
    
    This reverts commit a82b627dbae28a4ce2506f6cd29e5e3cc4ca7f66.
    
    reverting last of the faulty commits
    
    * Cleanup, re-add review modifications, prepare for merging
    
    * Prepare for merge
    
    * Add missing frontend files
    
    * Add missing backend files
    
    * Fix frontend bug with missing import statement
    
    * Remove unused code
    
    * Fixed third review notes
    
    Co-authored-by: = <mriperm@gmail.com>

--- sources/Debate/src/main/java/com/example/Debate/repository/DebateRepository.java
+++ sources/Debate/src/main/java/com/example/Debate/repository/DebateRepository.java
@@ -3,5 +3,10 @@ package com.example.Debate.repository;
 import com.example.Debate.model.Debate;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface DebateRepository extends MongoRepository<Debate,String> {
+
+    public List<Debate> findByTitleRegex(String reqName);
+
 }
