953d88d5b82d62e7392d45833daa5d5b0ab10c67
= <mriperm@gmail.com>
2020-05-13 21:12:06 +0200

    Code cleanup

--- sources/Debate/src/main/java/com/example/Debate/config/WebConfig.java
+++ sources/Debate/src/main/java/com/example/Debate/config/WebConfig.java
@@ -4,7 +4,6 @@ import com.example.Debate.common.api.StringToSortConverter;
 import com.example.Debate.common.api.StringToVoteConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,13 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToSortConverter());
         registry.addConverter(new StringToVoteConverter());
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/resources/");
     }
 
 }
