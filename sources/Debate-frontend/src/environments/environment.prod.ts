25f1aaec2808b9a2bd6b60b750247c218681da9f
= <mriperm@gmail.com>
2020-05-23 18:07:58 +0200

    Fix icons

--- sources/Debate-frontend/src/environments/environment.prod.ts
+++ sources/Debate-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  api: 'https://debater-api.azurewebsites.net/api'
+  // api: 'https://debater-api.azurewebsites.net/api'
+  api: 'http://localhost:8080/api'
 };
